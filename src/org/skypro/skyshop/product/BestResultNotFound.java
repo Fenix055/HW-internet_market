ecf57783158a064f981f327d960e248d7f9cafd6
Fenix055 <DevilHanter05@gmail.com>
2026-03-25 21:25:35 +0300

    Done 6

---
+++ src/org/skypro/skyshop/product/BestResultNotFound.java
@@ -0,0 +1,14 @@
+package org.skypro.skyshop.product;
+
+import java.io.IOException;
+
+public class BestResultNotFound extends IOException {
+    public BestResultNotFound() {
+        super();
+    }
+
+    public BestResultNotFound(String message) {
+        super(message);
+    }
+
+}
